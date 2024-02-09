917ed9c066316d8d7970f114e2b40a21f37b9257
Vipul Patil <vipulpatil@Vipuls-MacBook-Air.local>
2024-02-10 00:27:33 +0530

    Frontend Updated

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer flex justify-center'>
-      <p className='px-4'>Copyright @2024 IIT Ropar</p>
+      <p className='px-2 text-xl'>Copyright @2024 IIT Ropar</p>
     </div>
   )
 }
--- client/src/Components/Menu.jsx
+++ client/src/Components/Menu.jsx
@@ -3,25 +3,29 @@ import React from 'react'
 const Menu = () => {
   return (
     <div className="menu w-full flex justify-center pt-2">
-      <div className="menu-container w-7/12 px-6" style={{ background: 'linear-gradient(to bottom, white, #C7F4FF)' }}>
-        <ul className="tabs flex justify-end px-6 gap-4">
-          <li>
+      <div className="menu-container w-7/12 px-6" style={{ background: 'linear-gradient(to bottom, #E8F4FF, #C7F4FF)' }}>
+        <ul className="tabs flex justify-end gap-4">
+          <li className='px-1 text-xl'>
             <p>Home</p>
           </li>
            
-          <li>
+          <li className='px-1 text-xl'>
             <p>Dining</p>
           </li>
-          <li>
+          
+          <li className='px-1 text-xl'>
             <p>People</p>
           </li>
-          <li>
+
+          <li className='px-1 text-xl'>
             <p>Location</p>
           </li>
-          <li>
+
+          <li className='px-1 text-xl'>
             <p>Reservation</p>
           </li>
-          <li>
+
+          <li className='px-1 text-xl'>
             <p>Contact</p>
           </li>
         </ul>
