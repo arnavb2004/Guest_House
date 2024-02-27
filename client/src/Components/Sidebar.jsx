9eb5d54e8fd73744185569a84a8a67de94b1a090
Dhruv Singh Negi <2021csb1167@iitrpr.ac.in>
2024-02-27 22:25:23 +0530

    Added Registeration_Form and PDF Preview

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import styles from './Sidebar.module.css'; // Ensure CSS Module is correctly imported
 
@@ -13,13 +14,15 @@ const Sidebar = ({ isOpen }) => { // Accept isOpen as prop
 
   const content = user.role === 'ADMIN' ? 
   ["Home", "Pending Requests", "Contact"] : 
-  ["Home", "Register Form", "Services", "Contact"];
+  ["Home", "Reservation Form", "Services", "Contact"];
 
   return (
     <div className={`${styles.sidebar} ${!isOpen ? styles.closed : ''}`} style={{transform: isOpen ? 'translateX(0)' : 'translateX(-100%)'}}>
       <ul className={styles["sidebar-menu"]}>
         {content.map((item, index) => (
-          <li className={styles["menu-item"]} key={index}>{item}</li>
+          <li className={styles["menu-item"]} key={index}>
+          <Link to={`/${item.toLowerCase().replace(' ', '-')}`}>{item}</Link>
+          </li>
         ))}
       </ul>
     </div>
