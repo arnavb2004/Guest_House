1c575c14b10bebbd5cb31baa3680009bcbc7eb8d
Dhruv Singh Negi <2021csb1167@iitrpr.ac.in>
2024-03-13 00:40:22 +0530

    Improved BookDining Page and Cart Page

--- client/src/fooddata.js
+++ client/src/fooddata.js
@@ -1,120 +1,196 @@
-import food1 from "./images/pizza.jpeg";
-import food2 from "./images/burger.jpg";
-import food3 from "./images/burger.jpg";
-import food4 from "./images/pizza.jpeg";
+import food1 from "./images/Paratha.png";
+import food2 from "./images/Poha.png";
+import food3 from "./images/Dosa.png";
+import food4 from "./images/Upma.png";
 import food5 from "./images/pizza.jpeg";
-import food6 from "./images/pizza.jpeg";
+import food6 from "./images/burger.jpg";
+import food7 from "./images/pizza.jpeg";
+import food8 from "./images/pizza.jpeg";
+import food9 from "./images/pizza.jpeg";
+import food10 from "./images/pizza.jpeg";
+import food11 from "./images/pizza.jpeg";
+import food12 from "./images/pizza.jpeg";
+import food13 from "./images/pizza.jpeg";
+import food14 from "./images/pizza.jpeg";
+import food15 from "./images/pizza.jpeg";
+import food16 from "./images/pizza.jpeg";
+import food17 from "./images/pizza.jpeg";
+import food18 from "./images/pizza.jpeg";
+import food19 from "./images/GulabJamun.png";
+import food20 from "./images/Halwa.png";
+import food21 from "./images/Jalebi.png";
+import food22 from "./images/Tea.png";
+import food23 from "./images/Coffee.png";
+import food24 from "./images/Soup.png";
 
 export const menuItems1 = [
   {
     id: 1,
     image: food1,
-    price: 5.99,
-    name: "Pizza",
-    category: "Dinner",
+    price: 20,
+    name: "Paratha",
+    category: "Breakfast",
   },
   {
     id: 2,
     image: food2,
-    price: 6.99,
-    name: "Burger",
-    category: "Lunch",
+    price: 25,
+    name: "Poha",
+    category: "Breakfast",
   },
-
   {
     id: 3,
     image: food3,
-    price: 7.99,
-    name: "Paratha",
+    price: 50,
+    name: "Dosa",
     category: "Breakfast",
   },
   {
     id: 4,
     image: food4,
-    price: 9.99,
-    name: "Sandwich1",
+    price: 30,
+    name: "Upma",
     category: "Breakfast",
   },
   {
     id: 5,
     image: food5,
-    price: 15.99,
-    name: "Sandwich2",
+    price: 5.99,
+    name: "Pizza",
     category: "Breakfast",
   },
   {
     id: 6,
     image: food6,
-    price: 25.99,
-    name: "Sandwich3",
+    price: 5.99,
+    name: "Burger",
     category: "Breakfast",
   },
+  {
+    id: 7,
+    image: food2,
+    price: 5.99,
+    name: "Poha",
+    category: "Lunch",
+  },
+  {
+    id: 8,
+    image: food2,
+    price: 5.99,
+    name: "Poha",
+    category: "Lunch",
+  },
+  {
+    id: 9,
+    image: food2,
+    price: 5.99,
+    name: "Poha",
+    category: "Lunch",
+  },
+  {
+    id: 10,
+    image: food2,
+    price: 5.99,
+    name: "Poha",
+    category: "Lunch",
+  },
+  {
+    id: 11,
+    image: food2,
+    price: 5.99,
+    name: "Poha",
+    category: "Lunch",
+  },
+  {
+    id: 12,
+    image: food2,
+    price: 5.99,
+    name: "Poha",
+    category: "Lunch",
+  },
+  {
+    id: 13,
+    image: food2,
+    price: 5.99,
+    name: "Poha",
+    category: "Dinner",
+  },
+  {
+    id: 14,
+    image: food2,
+    price: 5.99,
+    name: "Poha",
+    category: "Dinner",
+  },
+  {
+    id: 15,
+    image: food2,
+    price: 5.99,
+    name: "Poha",
+    category: "Dinner",
+  },
+  {
+    id: 16,
+    image: food2,
+    price: 5.99,
+    name: "Poha",
+    category: "Dinner",
+  },
+  {
+    id: 17,
+    image: food2,
+    price: 5.99,
+    name: "Poha",
+    category: "Dinner",
+  },
+  {
+    id: 18,
+    image: food18,
+    price: 5.99,
+    name: "Poha",
+    category: "Dinner",
+  },
+  {
+    id: 19,
+    image: food19,
+    price: 10,
+    name: "GulabJamun",
+    category: "Dessert",
+  },
+  {
+    id: 20,
+    image: food20,
+    price: 20,
+    name: "Halwa",
+    category: "Dessert",
+  },
+  {
+    id: 21,
+    image: food21,
+    price: 20,
+    name: "Jalebi",
+    category: "Dessert",
+  },
+  {
+    id: 22,
+    image: food22,
+    price: 20,
+    name: "Tea",
+    category: "Drinks",
+  },
+  {
+    id: 23,
+    image: food23,
+    price: 40,
+    name: "Coffee",
+    category: "Drinks",
+  },
+  {
+    id: 24,
+    image: food24,
+    price: 50,
+    name: "Soup",
+    category: "Drinks",
+  },
 ];
 
-export const sliderItems2 = [
-  {
-    key: 1,
-    img: food1,
-    title: "Pizza",
-  },
-  {
-    key: 2,
-    img: food2,
-    title: "Burger",
-  },
-
-  {
-    key: 3,
-    img: food3,
-    title: "Paratha",
-  },
-  {
-    key: 4,
-    img: food4,
-    title: "Sandwich",
-  },
-  {
-    key: 5,
-    img: food5,
-    title: "Sandwich",
-  },
-  {
-    key: 6,
-    img: food6,
-    title: "Sandwich",
-  },
-];
-
-export const sliderItems3 = [
-  {
-    key: 1,
-    img: food1,
-    title: "Pizza",
-  },
-  {
-    key: 2,
-    img: food2,
-    title: "Burger",
-  },
-
-  {
-    key: 3,
-    img: food3,
-    title: "Paratha",
-  },
-  {
-    key: 4,
-    img: food4,
-    title: "Sandwich",
-  },
-  {
-    key: 5,
-    img: food5,
-    title: "Sandwich",
-  },
-  {
-    key: 6,
-    img: food6,
-    title: "Sandwich",
-  },
-];
