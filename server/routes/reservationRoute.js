e3fa1711775816f75782c3798b3eaa00836387d6
nishad-dhuri-05 <ndhuri24@gmail.com>
2024-03-19 12:18:29 +0530

    Added receipts field in the reservation model

--- server/routes/reservationRoute.js
+++ server/routes/reservationRoute.js
@@ -17,7 +17,10 @@ import {
 
 const Router = express.Router();
 
-Router.post("/", checkAuth, upload.array('files',10),createReservation);
+Router.post("/", checkAuth, upload.fields([
+  { name:'files',maxCount:5},
+  {name:'receipt',maxCount:1}
+]),createReservation);
 
 Router.get("/all", checkAuth, getAllReservationDetails);
 
