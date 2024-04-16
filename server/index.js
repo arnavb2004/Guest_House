89857fb9e5628483397f0a0baebf78d05b0d51c0
hardik <hardik32904@gmail.com>
2024-04-16 17:28:17 +0530

    Added pm2 config

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
 
 // var storage,upload;
 const connection = mongoose
-  .connect(process.env.MONGO_URL)
+  .connect("")
   .then(() => {
     console.log("Connected to database");
     // storage= new GridFsStorage({db:mongoose.connection.db})
