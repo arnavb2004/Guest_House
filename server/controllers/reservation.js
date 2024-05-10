d06503624a4d2d6c5a0689eb48f4d0657ef97f1d
hardik <hardik32904@gmail.com>
2024-05-10 17:37:17 +0530

    Fixes

--- server/controllers/reservation.js
+++ server/controllers/reservation.js
@@ -394,7 +394,7 @@ export async function rejectReservation(req, res) {
         .status(403)
         .json({ message: "You are not authorized to perform this action" });
     }
-    let found;
+    let found = false;
     reservation.reviewers = reservation.reviewers.map((reviewer) => {
       if (reviewer.role === req.user.role) {
         found = true;
