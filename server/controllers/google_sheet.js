8b9a2d289b54c48a2ff9e009a716eee7bac17d20
Arnav Bansal <bansalarnav1@gmail.com>
2025-03-10 15:39:06 +0530

    check

--- server/controllers/google_sheet.js
+++ server/controllers/google_sheet.js
@@ -5,7 +5,7 @@ const googleSheets = google.sheets("v4");
 const auth = new google.auth.JWT(
   process.env.client_email,
   null,
-  process.env.private_key.replace(/\\n/g, '\n'),
+  process.env.private_key,
   ["https://www.googleapis.com/auth/spreadsheets"]
 );
 
