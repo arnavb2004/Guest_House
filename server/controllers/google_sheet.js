829e854eaa7e3f6c18d342107608616436d6cc44
Arnav Bansal <bansalarnav1@gmail.com>
2025-03-10 17:58:28 +0530

    google sheets prob vercel

--- server/controllers/google_sheet.js
+++ server/controllers/google_sheet.js
@@ -5,7 +5,7 @@ const googleSheets = google.sheets("v4");
 const auth = new google.auth.JWT(
   process.env.client_email,
   null,
-  process.env.private_key,
+  process.env.private_key.replace(/\\n/g, '\n'),
   ["https://www.googleapis.com/auth/spreadsheets"]
 );
 
